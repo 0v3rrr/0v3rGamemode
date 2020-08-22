2b37eb4938db9cd852b015620ffb9b9db8f601dd
The0v3rD0z <60151455+The0v3rD0z@users.noreply.github.com>
2020-08-22 15:43:13 +0200

    Update Main.php

--- src/over/Main.php
+++ src/over/Main.php
@@ -1,19 +1,18 @@
 <?php
 
-namespace over; 
+namespace over;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\player\PlayerGameModeChangeEvent;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\command\ConsoleCommandSender;
 use pocketmine\utils\TextFormat;
 use pocketmine\Player;
 use pocketmine\event\Listener;
 
 class Main extends PluginBase implements Listener{
 
-public function onEnable(){
+public function onEnable()
+{
 $this->getLogger()->info("[PLUGIN 0v3rCore ON]");
 }
 
@@ -22,53 +21,45 @@ public function onCommand(CommandSender $sender, Command $cmd, string $label, ar
 
 switch($cmd->getName()){
 
-	case: "gm0"
-	
+	case "gm0":
+		if($sender instanceof Player){
+		if ($sender->hasPermission("overgm0.cmd")){
+		$sender->sendMessage("§7[§cOv3r§aGamemode§7] Vous avez été mis en Gamemode: survie.");
+		$sender->sendGamemode(0);
+		}else{
+		$sender->sendMessage("§7[§c0v3r§aGamemode§7] §cErreur vous n'avez pas la permission.");
+		}
+	break;
+	case "gm1":
 	if($sender instanceof Player){
-	if ($sender->hasPermission("overgm0.cmd")){
-	$sender->sendMessage("§7[§cOv3r§aGamemode§7] Vous avez été mis en Gamemode: survie.");
-	$sender->sendGamemode(0);
-	}else{
-	$sender->sendMessage("§7[§c0v3r§aGamemode§7] §cErreur vous n'avez pas la permission.");
-	}
-	}break;
-	
-	case: "gm1"
-	
+		if ($sender->hasPermission("overgm0.cmd")){
+		$sender->sendMessage("§7[§cOv3r§aGamemode§7] Vous avez été mis en Gamemode: créatif");
+		$sender->sendGamemode(1);
+		}else{
+		$sender->sendMessage("§7[§c0v3r§aGamemode§7] §cErreur vous n'avez pas la permission.");
+		}
+	break;
+	case "gm2":
 	if($sender instanceof Player){
-	if ($sender->hasPermission("overgm1.cmd")){
-	$sender->sendMessage("§7[§cOv3r§aGamemode§7] Vous avez été mis en Gamemode: créatif.");
-	$sender->sendGamemode(1);
-	}else{
-	$sender->sendMessage("§7[§c0v3r§aGamemode§7] §cErreur vous n'avez pas la permission.");
-	}
-	}break;
-	
-	case: "gm2"
-	
+		if ($sender->hasPermission("overgm0.cmd")){
+		$sender->sendMessage("§7[§cOv3r§aGamemode§7] Vous avez été mis en Gamemode: aventure.");
+		$sender->sendGamemode(2);
+		}else{
+		$sender->sendMessage("§7[§c0v3r§aGamemode§7] §cErreur vous n'avez pas la permission.");
+		}
+	break;
+	case "gm3":
 	if($sender instanceof Player){
-	if ($sender->hasPermission("overgm2.cmd")){
-	$sender->sendMessage("§7[§cOv3r§aGamemode§7] Vous avez été mis en Gamemode: aventure.");
-	$sender->sendGamemode(2);
-	}else{
-	$sender->sendMessage("§7[§c0v3r§aGamemode§7] §cErreur vous n'avez pas la permission.");
-	}
-	}break;
-	
-	case: "gm3"
-	
-	if($sender instanceof Player){
-	if ($sender->hasPermission("overgm3.cmd")){
-	$sender->sendMessage("§7[§cOv3r§aGamemode§7] Vous avez été mis en Gamemode: spectateur.");
-	$sender->sendGamemode(3);
-	}else{
-	$sender->sendMessage("§7[§c0v3r§aGamemode§7] §cErreur vous n'avez pas la permission.");
-	}
-	}break;
-	
-}
+		if ($sender->hasPermission("overgm0.cmd")){
+		$sender->sendMessage("§7[§cOv3r§aGamemode§7] Vous avez été mis en Gamemode: spectateur.");
+		$sender->sendGamemode(3);
+		}else{
+		$sender->sendMessage("§7[§c0v3r§aGamemode§7] §cErreur vous n'avez pas la permission.");
+		}
+	break;
+
+}return true;
 
 }
 
 }
-
